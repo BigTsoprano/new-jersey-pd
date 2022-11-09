fbd26116ac9432e91c6885abfbb3e946e3ae0771
shanon <shanonmambo@gmail.com>
2022-11-08 23:09:49 -0500

    again commit

--- src/components/sections/Products.js
+++ src/components/sections/Products.js
@@ -34,7 +34,7 @@ function Products() {
 
     <div className='product-section'>
         <motion.h1 animate={{ x: 500 }} transition={{ duration: 1.2 }}  >
-            PRODUCT SECTION
+            PRODUCT SECTIONs
         </motion.h1>
     <div className='products'>
        
